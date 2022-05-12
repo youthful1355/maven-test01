222e02cf1e9fbbe1377892dae5b974497d983d60
luzeyu02 <694523341@qq.com>
2022-05-12 15:41:39 +0800

    push commit2

--- src/main/java/com/meituan/git/GitTest.java
+++ src/main/java/com/meituan/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello, hot-fix2");
         System.out.println("hello, hf");
         System.out.println("push test");
+        System.out.println("push ssh test");
     }
 }
